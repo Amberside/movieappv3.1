5f4481ca1386a04566ca7c2a61d68fe74f010183
Amberle Seidl <47558882+Amberside@users.noreply.github.com>
2026-09-10 14:04:25 +1000

    Finished MovieList class

--- js/movie-list.js
+++ js/movie-list.js
@@ -63,7 +63,20 @@ class MovieList{
 
   /**
    * @function genMovieSearchList
+   * @description Generate a movie based on our search term
+   * @param {Array} list - The list of movies to display based on the search term
    */
+  genMovieSearchList(list){
+    // Remove all elements
+    this.removeElements();
+    // Generate a new list
+    for(let i = 0; i < list.length; i++){
+      let movie = list[i];
+      // Call the movieRow function
+      this.movieRow(movie.title, movie.year);
+    }
+  }
+
    /**
     * Remove all list elements for the DOM
     * This allows a new list to be displayed
@@ -97,23 +110,88 @@ class MovieList{
     this.removeElements();
     this.genMovieList();
   }
+  // CRUD - CREATE / READ / UPDATE / DELETE
+  /**
+   * Adding a new movie to the movieList - CREATE
+   * @function add
+   * @param {string} title - The movie title
+   * @param {number} year - The year the movie was released
+   */
+  add(title, year ){
+    // Add a new movie to the end of the list.
+    this.movieList.push({ title: title, year: year });
+    // Write use ES6 syntax 
+    // this.movieList.push({title, year });
+    this.refresh();
+  }
+  
+  /**
+   * Update a movie in the movieList
+   * @function update
+   * @param {number} index - The index of the movie to update
+   * @param {string} title - The new movie title
+   * @param {number} year - The new year for the movie
+   */
+  update(index, title, year){
+    // update the title
+    this.movieList[index].title = title;
+    // update the year
+    this.movieList[index].year = year;
+    // Refresh
+    this.refresh();
+  }
+
+  /**
+   * Delete a movie from the movieList - DELETE
+   * @function delete
+   * @param {number} index - The index of the movie to delete
+   */
+  delete(index){
+    // Remove one move form the movieList
+    this.movieList.splice(index, 1);
+    // refresh
+    this.refresh();
+  }
+
+  /**
+   * @function sortA2Z
+   * @description sort the movieList from A to Z, in ascending order
+   */
+  sortA2Z(){
+    this.movieList.sort(function(a, b){
+      return a.title.localeCompare(b.title);
+    });
+    this.refresh();
+  }
+
+  /**
+   * @function sortZ2A
+   * @description sort the movieList from Z to A, in descending order
+   */
+  sortZ2A(){
+    this.movieList.sort(function(a, b){
+      return b.title.localeCompare(a.title);
+    });
+    this.refresh();
+  }
 
    /**
-   * @function add
-   */
-   /**
-   * @function update
-   */
-   /**
-   * @function delete
-   */
-   /**
-   * @function sortA2Z
-   */
-   /**
-   * @function sortZ2A
-   */
-   /**
    * @function search
+   * @description Search the movieList titles for a partial match based on a search string. Will call the genMoveSearchList method when done to show the results in the UI.
+   * @param {string} nameString - The partial title to search for.
    */
+  search(nameString){
+    // Create a new list to hold search results
+    let shortList = [];
+    // Loop through the titles to see if the nameString is in a movieTitle
+    for (movie of this.movieList){
+      // Check to see if nameString is in movie.title
+      if(movie.title.includes(nameString)){
+        // add this movie to our shortList
+        shortList.push(movie);
+      }
+    }
+    // call the genMovieSearchList
+    this.genMovieSearchList(shortList);
+  }
 }
