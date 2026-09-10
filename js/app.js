6f375bef7ea51ad37beaf55a936d1b7132e234e9
Amberle Seidl <47558882+Amberside@users.noreply.github.com>
2026-09-10 11:44:38 +1000

    Finish the forms showing and hiding

---
+++ js/app.js
@@ -0,0 +1,46 @@
+/**
+ * @file app.js
+ * @description This file contains the JavaScript for our movie app.
+ * It contains the movieList instance, the event functions and UI code.
+ * 
+ * @author Amberle Seidl
+ * @version 3.1.0
+ * @since v3
+ * getData()
+ * onUpIndexChange()
+ * confirm code in the deleteClick()
+ * Bound checking for getData() and deleteClick()
+ */
+
+
+// UI Javascript
+/**
+ * JavaScript function for opening the forms
+ * @function openForm
+ * @param {object} evt - the event object.
+ * @param {string} action - The name of the action being used.
+ */
+function openForm(evt, action){
+  // declare variables
+  let i, tabContent, tabLinks;
+  // Get All elements that have the classname of tabcontent
+  tabContent = document.getElementsByClassName('tabcontent');
+  for( i =0; i < tabContent.length; i++){
+    // set the display to none for all elements with this class name
+    tabContent[i].style.display = 'none';
+  }
+  // get all elements that have the classname of tablinks
+  tabLinks = document.getElementsByClassName('tablinks');
+  for(i = 0; i < tabLinks.length; i++){
+    // change the classlist to remove the active class
+    tabLinks[i].className = tabLinks[i].className.replace("active", "");
+  }
+  // Show the current tab and add the active class to the button that opened the tab
+  document.getElementById(action).style.display = "block";
+  evt.currentTarget.className += " active"
+}
+// End of openForm()
+
+// Open a tab by default(
+document.getElementById('defaultOpen').click();
+
