5f92a67c817381327dfa1f9dfa91013f9aa23fe5
Amberle Seidl <47558882+Amberside@users.noreply.github.com>
2026-09-10 13:23:07 +1000

    Adding in methods movieRow, genMovieList, removeElements, refrest

--- js/movie-list.js
+++ js/movie-list.js
@@ -27,23 +27,74 @@ class MovieList{
   // Methods
 
   /**
+   * Generate one row of the movieList for display
+   * It will create the necessary HTML elements fro displaying a single movie to the UI
    * @function movieRow
+   * @param {string} title - The title of the movie
+   * @param {number} year - The year the movie was released
    */
+  movieRow(title, year){
+    // Get the parent element
+    const rootElement = document.getElementById(this.rootId);
+    // Create a new li
+    const row = document.createElement('li');
+    // Add the class of row to the li we just created
+    row.classList.add('row');
+    row.textContent = `${title} (${year})`;
+    // Add the li to the list
+    rootElement.appendChild(row);
+  }
+
   /**
+   * Generate all rows in our movieList (Read)
+   * This method will call movieRow for each element in our movieList.
+   * This will create all movies for our UI to display
    * @function genMovieList
    */ 
+  genMovieList(){
+    // Loop through the movieList
+    for(let i = 0; this.movieList.length; i++){
+      let movie = this.movieList[i];
+      // call the movieRow method.
+      this.movieRow(movie.title, movie.year);
+    }
+  }
+
   /**
    * @function genMovieSearchList
    */
    /**
+    * Remove all list elements for the DOM
+    * This allows a new list to be displayed
    * @function removeElements
    */
+  removeElements(){
+    // Getting the root ID
+    const rootElement = document.getElementById(this.rootId);
+    // Get all the elements with the class name of row.
+    const childNodes = document.getElementsByClassName('row');
+    // childNodes[0,1,2,3,4,5,6,7,8]
+    // How many children do we have?
+    const len = childNodes.length - 1;
+    for(let i = len; i >=0; i--){
+      // pull out the list child
+      const child = childNodes[i];
+      // Remove this child from the DOM
+      rootElement.removeChild(child);
+    }
+  }
    /**
    * @function getRow
    */
    /**
+    * Call the removeElements() method and 
+    * Call genMovieList() to add in the new list
    * @function refresh
    */
+  refresh(){
+    this.removeElements();
+    this.genMovieList();
+  }
    /**
    * @function add
    */
