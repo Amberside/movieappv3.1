903d74d74ee1d2977f9871e7a69c535b119325e1
Amberle Seidl <47558882+Amberside@users.noreply.github.com>
2026-09-10 12:04:29 +1000

    Start class definition for MovieList

---
+++ js/movie-list.js
@@ -0,0 +1,65 @@
+/**
+ * @file movie-list.js
+ * @description This file holds the class definition of our MovieList class
+ * cSpell: ignore Amberle Seidl
+ * @author Amberle Seidl
+ * @version 2.0.0
+ * @since v2
+ * getRow()
+ */
+
+/**
+ * MovieList Class
+ * This class has 2 properties and numerous methods
+ * Look at the Readme.md file for a fill list of methods
+ * @class MovieList
+ * @property {string} rootId - This is the id of the HTML element where the list is to be displayed
+ * @property {Array} movieList - The array of movies to be displayed (and stored)
+ * @property {function} refresh - The method to remove all current movies from the HTML document and display the current movieList.
+ */
+
+class MovieList{
+  constructor(rootId, movies){
+    this.rootId = rootId; // The HTML id where the list is going
+    this.movieList = movies  // The array of movies to be displayed
+    this.refresh();
+  }
+  // Methods
+
+  /**
+   * @function movieRow
+   */
+  /**
+   * @function genMovieList
+   */ 
+  /**
+   * @function genMovieSearchList
+   */
+   /**
+   * @function removeElements
+   */
+   /**
+   * @function getRow
+   */
+   /**
+   * @function refresh
+   */
+   /**
+   * @function add
+   */
+   /**
+   * @function update
+   */
+   /**
+   * @function delete
+   */
+   /**
+   * @function sortA2Z
+   */
+   /**
+   * @function sortZ2A
+   */
+   /**
+   * @function search
+   */
+}
